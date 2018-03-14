e2ccb40062dd5c37bca5148a39664c18bb614e88
Michael Eden <Mikey.Eden@gmail.com>
2018-03-14 00:57:44 -0400

    Added support for database?

--- W18/Week 11/Archery/app.py
+++ W18/Week 11/Archery/app.py
@@ -1,23 +1,30 @@
-from flask import Flask, request, render_template
-
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask import request, redirect, url_for
+from mongo import Database
 
 OK = "200"
 BAD_REQUEST = "400"
 SERVER_ERROR = "500"
 
-@app.route('/')
-def index():
-    return render_template('archery.html')
+DATABASE_NAME = "UserGames"
+SCORES = "Scores"
 
-@app.route('/score', methods=["GET", "POST"])
+app = Flask(__name__)
+db = Database(DATABASE_NAME)
+
+@app.route("/")
+def index():
+    return render_template("archery.html")
+
+@app.route("/score", methods=["GET", "POST"])
 def uploadScore():
     if request.method == "POST":
-        data = request.data
+        user_id = "dude"
+        score = request.data
 
-        # TODO: process data.
+        success = db.insert_one(SCORES, {user_id: score})
 
-        return OK
+        return OK if success else SERVER_ERROR
 
     elif request.method == "GET":
-        return render_template('archery.html'), OK
+        return redirect(url_for("/score"), code=302)
